be0ec8c8890c8dbadb9178e5bea6a711b81b95ef
SAZER\nokme <power4430@naver.com>
2023-01-13 11:50:45 +0900

    default project

---
+++ src/main/java/com/gaya/thegayap/TheGayaPApplication.java
@@ -0,0 +1,13 @@
+package com.gaya.thegayap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGayaPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheGayaPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaya/thegayap/TheGayaPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaya.thegayap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGayaPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
