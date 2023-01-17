e26ee5b61f30df57742d5262a92956cd150d3881
logidell <yuhiee123@gmail.com>
2023-01-17 16:36:22 +0900

    Hold - date picker 위치 조정 필요 - 2023.01.17

--- frontend/src/dellMain/MainDatePicker.jsx
+++ frontend/src/dellMain/MainDatePicker.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, forwardRef} from "react";
 import DatePicker from "react-datepicker";
 import {ko} from 'date-fns/esm/locale';
 import "react-datepicker/dist/react-datepicker.css";
@@ -8,6 +8,10 @@ const MainDatePicker = () => {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
 
+    const CustomInput = forwardRef(({value, onClick}, ref) => (
+        <button className="btn btn-outline-secondary" onClick={onClick} ref={ref}>
+            {value}{}
+        </button>));
 
     return (
 
@@ -21,25 +25,27 @@ const MainDatePicker = () => {
                     startDate={startDate}
                     endDate={endDate}
                     locale={ko}
+                    customInput={<CustomInput/>}
                 />
             </div>
 
             <div>
-                <div>
-                    <DatePicker
-                        dateFormat="yyyy-MM-dd"
-                        selected={endDate}
-                        onChange={(date) => setEndDate(date)}
-                        selectsEnd
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        locale={ko}
-                    />
-                </div>
+                <DatePicker
+                    dateFormat="yyyy-MM-dd"
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    locale={ko}
+                    customInput={<CustomInput/>}
+                />
             </div>
-        </div>
-    );
+
+        </div>);
 };
+
+
 
 export default MainDatePicker
