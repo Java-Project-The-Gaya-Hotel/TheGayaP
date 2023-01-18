438e115d70999d3838f45acaea6ba192cea18d54
logidell <yuhiee123@gmail.com>
2023-01-18 12:00:12 +0900

    no message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import DellReact from "./DellReact";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <DellReact/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <DellReact/>
+    </React.StrictMode>
 );
