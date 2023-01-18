3b47e73271f28c5ae7b9e1afc633c56c14c1780a
SAZER\nokme <power4430@naver.com>
2023-01-18 14:26:20 +0900

    로그인 페이지 틀 완성

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import DellReact from "./DellReact";
+import Login from "./login/Login";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <DellReact/>
+        <Login/>
     </React.StrictMode>
 );
--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
             <div id={"login-head"} className={"container-fluid col-md-10 mx-auto border-bottom pb-5"}>
                 <h4 className={"text-lg-start"}>로그인</h4>
             </div>
-            <div className={"d-grid justify-content-center col-md-8 mx-auto"}>
+            <div className={"grid justify-content-center col-md-8 mx-auto"}>
                 <div className={"text-center mt-5"}>
                     <h2>가야리워즈에 오신 것을 환영합니다.</h2>
                     <span>
@@ -24,25 +24,30 @@ function Login() {
                         <button type={"button"} className={"btn btn-outline-secondary"} style={{borderRadius: 0}}>비회원
                         </button>
                     </div>
-                    <div className={"container-fluid"}>
+                    <div className={"container-fluid p-5"}>
                         <div className={"d-grid justify-content-center"}>
-                            <div className={""}>
-                                <div className={"d-flex"}>
-                                    <div className={"d-grid"}>
-                                        <input type={"text"} className={""}/>
-                                        <input type={"text"} className={""}/>
+                            <div className={"d-grid"}>
+                                <div className={"row mt-2"}>
+                                    {/*아이디*/}
+                                    <div className={'col-10'}>
+                                        <input type={"text"} className={"col-11"}/>
+                                        {/*비밀번호*/}
+                                        <input type={"text"} className={"col-11"}/>
                                     </div>
-                                    <button className={"btn btn-secondary btn-lg mx-4"} style={{borderRadius: 0}}>로그인
-                                    </button>
+                                    <div className={'col-2 p-0 d-flex row'}>
+                                        <button className={"btn btn-secondary btn-lg"}
+                                                style={{borderRadius: 0}}>로그인
+                                        </button>
+                                    </div>
                                 </div>
-                                <div className={"d-flex"}>
+                                <div className={"d-flex my-2"}>
                                     <input type={"checkbox"}/>
-                                    <label className={""}>가야 리워즈 번호 또는 아이디 저장</label>
+                                    <label className={"ms-2"}>가야 리워즈 번호 또는 아이디 저장</label>
                                 </div>
                             </div>
                             <div className={"d-flex"}>
                                 <button className={"btn btn-dark"} style={{borderRadius: 0}}>가야 리워즈 가입</button>
-                                <button className={"btn btn-secondary"} style={{borderRadius: 0}}>가야 리워즈 번호 또는 아이디 찾기
+                                <button className={"btn btn-secondary mx-1"} style={{borderRadius: 0}}>가야 리워즈 번호 또는 아이디 찾기
                                 </button>
                                 <button className={"btn btn-secondary"} style={{borderRadius: 0}}>비밀번호 찾기</button>
                             </div>
@@ -50,7 +55,7 @@ function Login() {
                     </div>
                 </div>
                 <div className={"bg-opacity-10 bg-dark text-center mt-3"}>
-                    <p className={"text-center m-2"}>이메일, 연락처 등의 정보가 변경되면 웹사이트에서 회원정보를 수정해주시기 바랍니다.</p>
+                    <p className={"text-center mx-2 my-2 py-2"}>이메일, 연락처 등의 정보가 변경되면 웹사이트에서 회원정보를 수정해주시기 바랍니다.</p>
                 </div>
             </div>
         </div>
