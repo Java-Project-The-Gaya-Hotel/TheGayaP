7eeadc42a510b1505fb1866522cb0ba5f7d552fb
황하늘 <>
2023-02-05 16:18:50 +0900

    회원가입 유효성 검사

--- frontend/src/join/Join.jsx
+++ frontend/src/join/Join.jsx
@@ -1,9 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import "./Join.css"
 import axios, {request} from "axios";
+import button from "bootstrap/js/src/button";
 
+
+function FootButton(props: { disabled: boolean, type: string, footButtonType: *, children: ReactNode }) {
+    return null;
+}
 
 function Join(props) {
+
 
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
@@ -13,12 +19,23 @@ function Join(props) {
     const [pw, setPw] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
 
+    // 오류 메세지
     const [idMes, setIdMes] = useState("")
     const [emailMes, setEmailMes] = useState("")
     const [nameMes, setNameMes] = useState("")
     const [numberMes, setNumberMes] = useState("")
     const [pwMes, setPwMes] = useState("")
     const [confirmPasswordMes, setConfirmPasswordMes] = useState("")
+    const [birthMes, setBirthMes] = useState("")
+
+    //유효성 검사
+    const [isName, setIsName] = useState(false)
+    const [isId, setIsId] = useState(false)
+    const [isNumber, setIsNumber] = useState(false)
+    const [isEmail, setIsEmail] = useState(false)
+    const [isPassword, setIsPassword] = useState(false)
+    const [isConfirmPassword, setIsConfirmPassword] = useState(false)
+    // const router = useRouter()
 
     const data = {
         memberName: name,
@@ -33,31 +50,78 @@ function Join(props) {
         setBirth(e.target.value)
     }
 
-    const onNameHandler = (e) => {
-        setName(e.target.value)
-    }
+    const onNameHandler= useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const nameRegex =  /^(?=.*[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{2,5}$/
+        const nameCurrent = e.target.value
+        setName(nameCurrent)
 
-    const onNumberHandler = (e) => {
+        if (!nameRegex.test(nameCurrent)) {
+            setNameMes('한글로 2글자 이상 5글자 미만으로 입력해주세요.')
+            setIsName(false)
+        } else {
+            setNameMes('')
+            setIsName(true)
+        }
+    }, [])
+
+    const onNumberHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setNumber(e.target.value)
+        if (e.target.value.length < 12 || e.target.value.length > 13){
+            setNumberMes('올바른 전화번호를 입력해주세요')
+            setIsNumber(false)
+        }else {
+            setNumberMes("")
+            setIsNumber(true)
+        }
+    }, [])
+    // const onEmailHandler = (e) => {
+    //     setEmail(e.target.value)
+    // }
+    //
+    const onIdHandler= useCallback((e: React.ChangeEvent<HTMLInputElement>) =>{
+        const idRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{4,25}$/
+        const idCurrent = e.target.value
+        setId(idCurrent)
 
-    }
-    const onEmailHandler = (e) => {
-        setEmail(e.target.value)
-    }
+        if (!idRegex.test(idCurrent)) {
+            setIdMes('숫자+영문자 조합으로 4자리 이상 입력해주세요!')
+            setIsId(false)
+        } else {
+            setIdMes('')
+            setIsId(true)
+        }
+    },[])
 
-    const onIdHandler = (e) => {
-        setId(e.target.value)
+    const onPasswordHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) =>{
+            const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,25}$/
+            const passwordCurrent = e.target.value
+            setPw(passwordCurrent)
+
+        if (!passwordRegex.test(passwordCurrent)) {
+            setPwMes('숫자+영문자 조합으로 6자리 이상 입력해주세요!')
+            setIsPassword(false)
+        } else {
+            setPwMes('')
+            setIsPassword(true)
+        }
+    },[])
+
+    const onConfirmPasswordHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>)  => {
+            const passwordConfirmCurrent = e.target.value
+            setConfirmPassword(passwordConfirmCurrent)
+
+            if (pw === passwordConfirmCurrent) {
+                setConfirmPasswordMes('비밀번호를 똑같이 입력했어요 : )')
+                setIsConfirmPassword(true)
+            } else {
+                setConfirmPasswordMes('비밀번호가 틀려요. 다시 확인해주세요')
+                setIsConfirmPassword(false)
+            }
+        },
+        [pw]
+    )
 
 
-    }
-
-    const onPasswordHandler = (e) => {
-        setPw(e.target.value)
-    }
-
-    const onConfirmPasswordHandler = (e) => {
-        setConfirmPassword(e.target.value)
-    }
     const onSubmitHandler = (event) => {
         event.preventDefault(); //리프레시 방지-> 방지해야 이 아래 라인의 코드들 실행 가능
 
@@ -76,6 +140,22 @@ function Join(props) {
         })
 
     }
+    const onChangeEmail = (e) => {
+        const currentEmail = e.target.value;
+        setEmail(currentEmail);
+        const emailRegExp =
+            /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+
+        if (!emailRegExp.test(currentEmail)) {
+
+            setEmailMes("");
+            setIsEmail(false);
+        } else {
+            setEmailMes("");
+            setIsEmail(true);
+        }
+    };
+
     const joinBtn = () => {
         if (id && name && email && number && pw && confirmPassword) {
             alert('회원 가입 완료');
@@ -98,6 +178,9 @@ function Join(props) {
             if (!number) {
                 setNumberMes("전화번호을(를) 입력해주세요");
             }
+            if(!birth){
+                setBirthMes("생일을(를) 체크해주세요")
+            }
 
         }
     }
@@ -107,37 +190,54 @@ function Join(props) {
     const handleIdCheck = (e) => {
         e.preventDefault();
         console.log(id);
+        if (!id) {
+            setIdMes("아이디을(를)  입력해주세요");
+        }else {
+            axios.get("http://localhost:8080/join/idCheck",
+                {
+                    params: {memberId: id}
+                })
 
 
-        axios.get("http://localhost:8080/join/idCheck",
-            {
-                params: {memberId: id}
+                .then((req) => {
+                    console.log("데이터 전송 성공")
+
+                    if (req.data === 1) alert('중복된 아이디입니다.');
+                    else if (req.data === 0) {
+                        alert('사용가능한 아이디입니다.');
+                        setIdMes("")
+                    }
+
+                }).catch(err => {
+                console.log(`데이터 전송 실패 ${err}`)
             })
 
-            .then((req) => {
-                console.log("데이터 전송 성공")
+        }
 
-                if (req.data === 1) alert('중복된 아이디입니다.');
-                else if (req.data === 0) alert('사용가능한 아이디입니다.');
-            }).catch(err => {
-            console.log(`데이터 전송 실패 ${err}`)
-        })
+
     }
 //이메일 중복체크
     const handleEmailCheck = (e) => {
         e.preventDefault();
         console.log(email);
-        axios.get("http://localhost:8080/join/emailCheck",
-            {
-                params: {memberEmail: email}
+        if (!email) {
+            setEmailMes("이메일을(를)  입력해주세요");
+        }else {
+            axios.get("http://localhost:8080/join/emailCheck",
+                {
+                    params: {memberEmail: email}
+                })
+                .then((req) => {
+                    console.log("데이터 전송 성공")
+                    if (req.data === 1) alert('중복된 이메일입니다.');
+                    else if (req.data === 0){
+                        alert('사용가능한 이메일입니다.');
+                        setEmailMes("")
+                    }
+                }).catch(err => {
+                console.log(`데이터 전송 실패 ${err}`)
             })
-            .then((req) => {
-                console.log("데이터 전송 성공")
-                if (req.data === 1) alert('중복된 이메일입니다.');
-                else if (req.data === 0) alert('사용가능한 이메일입니다.');
-            }).catch(err => {
-            console.log(`데이터 전송 실패 ${err}`)
-        })
+        }
     }
     // const autoHyphen2 = (target) => {
     //     target.value = target.value
@@ -180,7 +280,7 @@ function Join(props) {
 
                                 <tr>
                                     <td> ID :</td>
-                                    <td><input type={"text"} value={id} onChange={onIdHandler} className={"id"} autoComplete={"off"}/></td>
+                                    <td><input type={"text"} value={id}  className={"id"} onChange={onIdHandler} autoComplete={"off"}/></td>
                                     <td>
                                         <button onClick={handleIdCheck} className={"btn btn-primary"}>아이디중복</button>
                                     </td>
@@ -189,7 +289,7 @@ function Join(props) {
 
                                 <tr>
                                     <td> E-Mail :</td>
-                                    <td><input type={"Email"} value={email} onChange={onEmailHandler}/></td>
+                                    <td><input type={"email"} value={email} onChange={onChangeEmail}/></td>
                                     <td>
                                         <button onClick={handleEmailCheck} className={"btn btn-primary"}>이메일 중복</button>
                                     </td>
@@ -205,7 +305,7 @@ function Join(props) {
 
                                 <tr>
                                     <td> PW Check :</td>
-                                    <td><input type={"password"} value={confirmPassword} onChange={onConfirmPasswordHandler}/></td>
+                                    <td><input type={"password"} value={confirmPassword} onChange={onConfirmPasswordHandler} /></td>
                                     <td><span className={"ast"}> {confirmPasswordMes}</span></td>
                                 </tr>
 
@@ -235,7 +335,7 @@ function Join(props) {
                                 <tr>
                                     <td> 생년월일 :</td>
                                     <td colSpan={2}><input type={"date"} value={birth} onChange={onBirthHandler} className={"Birth"}/></td>
-                                    <td><span>{nameMes}</span></td>
+                                    <td><span>{birthMes}</span></td>
                                 </tr>
 
 
@@ -250,7 +350,7 @@ function Join(props) {
                                             <option value="+81" title="+81">+81 일본</option>
                                             <option value="+84" title="+84">+84 중국</option>
                                         </select></td>
-                                    <td><input className={"col-2"} type={"text"} maxLength={13} value={number} onChange={onNumberHandler}/></td>
+                                    <td><input className={"col-6"} type={"text"} maxLength={13} onChange={onNumberHandler} value={number} /></td>
                                     <td><span>{numberMes}</span></td>
                                 </tr>
                                 </tbody>
@@ -258,9 +358,13 @@ function Join(props) {
                         </div>
 
                     </div>
-                    <button formAction={""} className={"btn btn-primary"} onClick={joinBtn}>
-                        회원 가입
-                    </button>
+                    <section>
+                        <button type={"submit"} onClick={joinBtn} className={"activation"} disabled={!(isEmail && isId &&
+                            isName && isNumber && isPassword && isConfirmPassword)}
+                        >
+                            다음
+                        </button>
+                    </section>
                 </div>
 
             </form>
