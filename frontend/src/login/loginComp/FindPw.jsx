493bfa24395807ca7444c47566b193425062a338
SAZER\nokme <power4430@naver.com>
2023-02-13 15:22:54 +0900

    이메일 인증 처리중

--- frontend/src/login/loginComp/FindPw.jsx
+++ frontend/src/login/loginComp/FindPw.jsx
@@ -13,9 +13,9 @@ function FindPw(props) {
     const [emailConfirmCode, setEmailConfirm] = useState("");
     const [memberConfirmCode, setMemberConfirmCode] = useState("");
     const [memberPw, setMemberPw] = useState("");
+    const [confirmPw, setConfirmPw] = useState("");
 
     const [isPassEmail, setIsPassEmail] = useState(true);
-
 
 
     function closeModal() {
@@ -31,7 +31,6 @@ function FindPw(props) {
             })
 
             .then((req) => {
-                console.log(req.data);
                 if (req.data === "fail") {
                     Swal.fire("일치하는 유저가 존재하지 않습니다.")
                 } else {
@@ -42,6 +41,45 @@ function FindPw(props) {
             }).catch(err => {
             Swal.fire("네트워크 오류가 발생했습니다.")
         })
+    }
+
+    // 인증 번호 확인
+    const compareCheckCode = () => {
+        if (emailConfirmCode === memberConfirmCode) {
+            Swal.fire({html:"인증이 성공했습니다<br/>비밀번호를 변경해 주세요."}).then(res => {
+                if (res.isConfirmed) {
+                    setIsPassEmail(false);
+                }
+            })
+        } else {
+            Swal.fire("인증번호가 일치하지 않습니다.");
+        }
+    }
+
+    // 비밀번호 변경
+    const changePw = () => {
+        if (memberPw !== confirmPw) {
+            Swal.fire('변경하실 비밀번호가 일치하지 않습니다.')
+
+        } else {
+
+            axios.put("http://localhost:8080/mypage/changepw",
+                {
+                    memberId: memberId,
+                    memberPw: memberPw
+                },
+            )
+                .then((req) => {
+                    Swal.fire(' 비밀번호가 변경됐습니다. ').then(res => {
+                        if (res.isConfirmed) {
+                            props.closeModal();
+                        }
+                    })
+                }).catch(err => {
+                Swal.fire('오류가 발생하였습니다, 다시 시도해 주세요.')
+            })
+        }
+
     }
 
 
@@ -72,23 +110,28 @@ function FindPw(props) {
 
                     <div className={"row mb-1 me-2"}>
                         <label className={"form-label col-sm-3 my-auto fw-bold"}> 인증 확인 </label>
-                        <input type={"text"} className={"form-control rounded-0 col-sm m-2"} onChange={(e)=>{setMemberConfirmCode()}}/>
+                        <input type={"text"} className={"form-control rounded-0 col-sm m-2"} onChange={(e) => {
+                            setMemberConfirmCode(e.target.value)
+                        }}/>
                         <button id={"userBtn"}
-                                className={"form-label col-sm-3 my-auto rounded-0 custom-btn3 btn-Login"}>인증 확인
+                                className={"form-label col-sm-3 my-auto rounded-0 custom-btn3 btn-Login"}
+                                onClick={compareCheckCode}>인증 확인
                         </button>
                     </div>
-
-
-                    <button className={"mt-3 rounded-0 custom-btn3 btn-Login"}>확인</button>
                 </div> :
                 <div>
+                    <div className={"d-grid justify-content-center p-5"}>
+                        <label className={"form-label col my-auto fw-bold"}> 변경 하실 비밀번호 </label>
+                        <input type={"text"} className={"rounded-0 col-sm m-2"} id={"changePw"} onChange={(e) => {
+                            setMemberPw(e.target.value)
+                        }}/>
+                        <label className={"form-label col my-auto fw-bold"}> 비밀번호 확인 </label>
+                        <input type={"text"} className={"rounded-0 col-sm m-2"} id={"confirmPw"}  onChange={(e) => {
+                            setConfirmPw(e.target.value)
+                        }}/>
+                    </div>
+                    <button onClick={changePw} className={"mt-4 rounded-0 custom-btn3 btn-Login"}>확인</button>
 
-                        <div>
-
-                        </div> :
-
-
-                    <button onClick={closeModal} className={"mt-4 rounded-0 custom-btn3 btn-Login"}>확인</button>
                 </div>
             }
         </div>
